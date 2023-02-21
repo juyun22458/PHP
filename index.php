699f57d405beb5237f9a95250c778aba3474e920
juyun22458 <125621559+juyun22458@users.noreply.github.com>
2023-02-22 01:07:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+<title>高大資管</title>
+
+</head>
+    
+<body>
+Hello HTML!<br/>
+<?php
+
+echo "Hello PHP";
+
+$i=8;
+$j=18;
+$x=$i*$j;
+echo "<br/>";
+echo $x;
+
+?>
+
+</body>
+    
+</html>
